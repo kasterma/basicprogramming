8d2f834b51f6b1ee8cd48ba44aac95774c2f14ad
Bart Kastermans <kasterma@kasterma.net>
2014-12-25 10:54:46 +0100

    testsetup

--- hackerrank/ai/bot-building/bot-saves-2/src/Solution.java
+++ hackerrank/ai/bot-building/bot-saves-2/src/Solution.java
@@ -43,8 +43,8 @@ public class Solution {
 	
 	public static void main(String[] args) {
 		try {
-			//Scanner in = new Scanner(new File(testCaseFile));
-			Scanner in = new Scanner(System.in);
+			Scanner in = new Scanner(new File(testCaseFile));
+			//Scanner in = new Scanner(System.in);
 			
 			int n,r,c;
 			n = in.nextInt();
