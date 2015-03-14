1f65cc85bf34150c6bc9163aa61a975160361cce
Bart Kastermans <kasterma@kasterma.net>
2015-03-14 15:15:06 +0100

    refactored clojure solution superman

--- hackerrank/week-14/superman/src/core.clj
+++ hackerrank/week-14/superman/src/core.clj
@@ -17,41 +17,47 @@
 
 ;;(trace/trace-vars step)
 
+(defn get-two-largest
+  "Given list of nonnegative numers (more than two) return pair
+   [largest next-largest]"
+  [xs]
+  (reduce
+    (fn [[l1 l2] i] (cond (< l1 i) [i l1]
+                          (< l2 i) [l1 i]
+                          :else [l1 l2]))
+    [0 0]
+    xs))
+
 (defn get-max [floor-cts I]
   (let [ground-floors (mapv (comp vector first) floor-cts)
         num-bld       (count floor-cts)
+        num-floor     (count (first floor-cts))
         new-val
-        (fn [acc building floor]
-         (if (<= I floor) ;; can jump down I
+        (fn [acc building floor [l1 l2]]
           (+ (get-in floor-cts [building floor])
-                (loop [max-sofar (get-in acc [building (dec floor)])
-                       b 0]
-                 (if (= b num-bld)
-                  max-sofar
-                  (if (= b building)
-                    (recur max-sofar (inc b))
-                    (recur (max max-sofar (get-in acc [b (- floor I)])) (inc b))))))
-          (+ (get-in floor-cts [building floor])
-             (get-in acc [building (dec floor)]))))
+            (max (cond
+                   (< floor I) 0  ;; cannot just down I floors
+                   (= l1 (get-in acc [building (- floor I)])) l2
+                   :else l1)
+                 (get-in acc [building (dec floor)]))) )
         add-floors
-        (fn [acc f]
-          (reduce (fn [acc b]
-                     (prof/p :ui (assoc!
-                      acc
-                      b
-                      (conj (get acc b) (prof/p :nv (new-val acc b f))))))
-                  acc
-                  (range (count floor-cts))))
-        ]
-    ;;(println floor-cts)
-    (persistent! (reduce add-floors
-      (transient ground-floors)
-      (range 1 (count (first floor-cts))) ;; start at 1 since already have ground floor
-      ))))
+        (fn [acc floor]
+          (let [ls   (if (<= I floor)
+                       (get-two-largest (mapv (fn [b] (get b (- floor I))) acc))
+                       [0 0])
+                newv (fn [acc b] (new-val acc b floor ls))]
+            (reduce (fn [acc b]
+                       (update-in acc [b] (fn [x] (conj x (newv acc b)))))
+                    acc
+                    (range num-bld))))]
+    (reduce add-floors
+      ground-floors
+      (range 1 num-floor) ;; start at 1 since already have ground floor
+      )))
 
 (defn main []
   (let [[N H I]      (get-ints)
-        floor-info   (prof/p :read (doall (for [i (range N)] (vec (rest (get-ints))))))
+        floor-info   (doall (for [i (range N)] (vec (rest (get-ints)))))
         building-cts (fn [idxs]
                        (persistent!
                         (reduce
@@ -59,11 +65,10 @@
                           (assoc! acc (dec idx) (inc (get acc (dec idx)))))
                          (transient (vec (repeat H 0)))
                          idxs)))
-        floor-cts    (prof/p :floors (mapv building-cts floor-info))]
+        floor-cts    (mapv building-cts floor-info)]
   ;;(println [N H I floor-info floor-cts])
-  ;;(println (get-max floor-cts I))
-  (println (prof/p :max (apply max (map last (get-max floor-cts I)))))))
-;; 2
-;; 3
+  (println (get-max floor-cts I))
+  (println (apply max (map last (get-max floor-cts I))))))
+;;
 (binding [*in* (java.io.BufferedReader. (java.io.FileReader. "testcase.txt"))]
-  (prof/profile :info :xx (main)))
+ (main))
